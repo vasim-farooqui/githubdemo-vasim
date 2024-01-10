a0a5f878f3862c77507818e42a0320a6575c2408
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-10 20:25:11 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,10 +1,8 @@
 #!groovy
-
-def deployOnLAMBDA () {
-    sh  """
-        echo "Deploying to Lambda" 
-		  
-        aws lambda update-function-code --function-name $lamfunction_name --zip-file $lambdafilepath    
+//def deployOnLAMBDA () {
+    //sh  """
+        echo "Deploying to Lambda" 	  
+      //  aws lambda update-function-code --function-name $lamfunction_name --zip-file $lambdafilepath    
         
-    """
-}
+    //"""
+//}

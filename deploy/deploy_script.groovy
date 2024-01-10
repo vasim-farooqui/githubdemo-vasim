91a72b663ed26fb90cc9103f834ac7e06af1afbd
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-10 15:59:49 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,33 +1,28 @@
-pipeline {
-    agent any
+#!groovy
 
-    environment {
+pipeline {
+
+  agent { 
+    node {
+      label 'DEPLOY_TEST_LAMBDA'
+    }
+  }
+	
+  environment {
         //AWS_ACCESS_KEY_ID = credentials('YOUR_AWS_ACCESS_KEY_ID')
         //AWS_SECRET_ACCESS_KEY = credentials('YOUR_AWS_SECRET_ACCESS_KEY')
         AWS_DEFAULT_REGION = 'us-east-1'
         lamfunction-name = 'Jenkins-deployment-lamda'
         LAMBDA_HANDLER = 'jenkins'
         lambdafilepath = '/home/lrnqa/jenkins/workspace/Test-Lambda/lamdbacode.zip' // Path to your updated Lambda function code
-    }
+    }  
+ } 
 
-    stages {
-        stage('Update Lambda Function') {
-            steps {
-                script {
-                    // Configure AWS CLI credentials
-
-                     {
-                        // Update Lambda function code
-                        sh "aws lambda update-function-code --function-name ${env.lamfunction-name} --zip-file fileb://${env.lambdafilepath}"
-
-                        // Publish a new version of the Lambda function
-                        def result = sh(script: "aws lambda publish-version --function-name ${env.lamfunction-name}", returnStdout: true).trim()
-                        def version = sh(script: "echo ${result} | jq -r '.Version'", returnStdout: true).trim()
-                        println "New version published: $version"
-                    }
-                }
-            }
-        }
-    }
+def loadCourseVersion () {
+    sh """
+        set +x
+        aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
+        aws lambda publish-version --function-name $lamfunction_name     
+	
+    """
 }
-

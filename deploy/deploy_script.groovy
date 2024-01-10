d0be77b1dde9b0e2d350cc37b904d0fba822f1e7
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-10 18:05:39 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,20 +1,36 @@
+[6:04 PM] Rakshith Ramesha
 pipeline {
+
     agent any
  
     environment {
+
         lamfunction_name = 'Jenkins-deployment-lamda'
+
         lambdafilepath = '/home/lrnqa/jenkins/workspace/Test-Lambda/lamdbacode.zip'
+
     }
  
     stages {
+
         stage("deployOnLAMBDA") {
+
             steps {
+
                 script {
+
                     sh '''
+
                         aws lambda update-function-code --function-name $lamfunction_name --zip-file $lambdafilepath
+
                     '''
+
                 }
+
             }
+
         }
+
     }
+
 }

e84e60448b18085c1a817087b69c172f7b807324
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-10 15:27:24 +0530

    Update deploy_script.groovy
    new code

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,16 +1,35 @@
-def deployOnLAMBDA () {
-  //check
-  //******** Following Variables need to come from jenkins ********
-  //ECS_CLUSTER=qa-lrn-com
-  //ECS_SERVICE=lrn-legacy-cm-email-service
-  //TASK_DEFINATION=legacy-cm-email-service
-  //AWS_REGION=us-east-1
-  // Testing Jenkins Jira integration
-  lamfunction_name = "Jenkins-deployment-lamda";
-  lambdafilepath = "/home/lrnqa/jenkins/workspace/Test-Lambda";
-  sh ( label:"Deploying to Lambda", script: """
-        set +x
-  aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
-  aws lambda publish-version --function-name $lamfunction_name
-   """)
+```groovy
+pipeline {
+    agent any
+
+    environment {
+        //AWS_ACCESS_KEY_ID = credentials('YOUR_AWS_ACCESS_KEY_ID')
+        //AWS_SECRET_ACCESS_KEY = credentials('YOUR_AWS_SECRET_ACCESS_KEY')
+        AWS_DEFAULT_REGION = 'us-east-1'
+        lamfunction-name = 'Jenkins-deployment-lamda'
+        LAMBDA_HANDLER = 'jenkins'
+        lambdafilepath = '/home/lrnqa/jenkins/workspace/Test-Lambda/lamdbacode.zip' // Path to your updated Lambda function code
+    }
+
+    stages {
+        stage('Update Lambda Function') {
+            steps {
+                script {
+                    // Configure AWS CLI credentials
+
+                     {
+                        // Update Lambda function code
+                        sh "aws lambda update-function-code --function-name ${env.lamfunction-name} --zip-file fileb://${env.lambdafilepath}"
+
+                        // Publish a new version of the Lambda function
+                        def result = sh(script: "aws lambda publish-version --function-name ${env.lamfunction-name}", returnStdout: true).trim()
+                        def version = sh(script: "echo ${result} | jq -r '.Version'", returnStdout: true).trim()
+                        println "New version published: $version"
+                    }
+                }
+            }
+        }
+    }
 }
+```
+

65ce88a8e72496433c1d98f3eab50f2e65d6eb02
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-11 15:55:26 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,1 +1,14 @@
-echo "test"
+def functionName = 'deployOnLAMBDA'
+
+node('DEPLOY_TEST_LAMBDA'){
+
+    stage('Deploy'){
+        echo "deploy"
+    }
+    
+}
+
+def deployOnLAMBDA () {
+    sh 'aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
+        aws lambda publish-version --function-name $lamfunction_name'
+}

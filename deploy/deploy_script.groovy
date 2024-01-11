d82283fb57814cd974af170c42a19cb44cd9faa2
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-11 16:14:03 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,14 +1,41 @@
-def functionName = 'deployOnLAMBDA'
-
-node('DEPLOY_TEST_LAMBDA'){
-
-    stage('Deploy'){
-        echo "deploy"
+pipeline {
+    agent any
+ 
+    environment {
+        LAMBDA_FUNCTION_NAME = 'Jenkins-deployment-lamda'
+        ZIP_FILE_PATH = '/home/lrnqa/jenkins/workspace/Test-Lambda/lamdbacode.zip'
     }
-    
-}
-
-def deployOnLAMBDA () {
-    sh 'aws lambda update-function-code --function-name $lamfunction_name --zip-file $lambdafilepath
-        aws lambda publish-version --function-name $lamfunction_name'
+ 
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout scm
+            }
+        }
+ 
+        stage('Update Lambda Function') {
+            steps {
+                script {
+                    // Assuming your AWS CLI is configured on the Jenkins instance with an IAM role that has Lambda update permissions
+                    sh "aws lambda update-function-code --function-name ${LAMBDA_FUNCTION_NAME} --zip-file fileb://${ZIP_FILE_PATH}"
+                }
+            }
+        }
+ 
+        stage('Cleanup') {
+            steps {
+                script {
+                    // Clean workspace after the build
+                    cleanWs()
+                }
+            }
+        }
+    }
+ 
+    post {
+        always {
+            // Clean up even if the build fails
+            cleanWs()
+        }
+    }
 }

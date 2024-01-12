8ca970d6435555154868f9586fd27e7b8d4ec03a
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-12 17:55:45 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,15 +1,22 @@
-def deployOnLAMBDA () {
-    echo "Deploying to Lambda"
-}
-  node {
-    label 'DEPLOY_TEST_LAMBDA'
-    stage('Deploy') {
-        sh ( label:"Deploying to Lambda", script: """
-        set +x
-  aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
-  aws lambda publish-version --function-name $lamfunction_name
-   """)
+pipeline {
+    agent any
+    parameters {
+        string(name: 'x1', defaultValue: 'default', description: 'This is a parameter')
+    }
+    stages {
+        stage('Deploy') {
+            steps {
+                script {
+                    def deployOnLAMBDA () {
+                        sh ( label:"Deploying to Lambda", script: """
+                             set +x
+                             aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
+                             aws lambda publish-version --function-name $lamfunction_name
+                            """)
+                    }
+                    depScript.deployOnLAMBDA();
+                }
+            }
+        }
     }
 }
- 
-    

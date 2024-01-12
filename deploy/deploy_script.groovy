470c8fe4f07b3473ab3b05bc59906f391fb78df9
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-12 18:03:57 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,22 +1,9 @@
-pipeline {
-    agent any
-    parameters {
-        string(name: 'x1', defaultValue: 'default', description: 'This is a parameter')
-    }
-    stages {
-        stage('Deploy') {
-            steps {
-                script {
-                    def deployOnLAMBDA () {
+def deployOnLAMBDA () {
                         sh ( label:"Deploying to Lambda", script: """
                              set +x
                              aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
                              aws lambda publish-version --function-name $lamfunction_name
                             """)
                     }
-                    depScript.deployOnLAMBDA();
-                }
-            }
-        }
-    }
-}
+         
+                

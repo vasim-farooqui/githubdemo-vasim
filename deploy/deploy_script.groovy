495483b71291760df82bb7ef0c38c372deec705b
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-12 16:39:36 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -23,10 +23,18 @@ def function_name
 } 
 
 def deployOnLAMBDA () {
-    sh """
-        echo "Deploying to Lambda" 
-		  
-		aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath   
-        
-    """
+    //check
+  //******** Following Variables need to come from jenkins ********
+  //ECS_CLUSTER=qa-lrn-com
+  //ECS_SERVICE=lrn-legacy-cm-email-service
+  //TASK_DEFINATION=legacy-cm-email-service
+  //AWS_REGION=us-east-1
+  // Testing Jenkins Jira integration
+  //lamfunction_name = "Jenkins-deployment-lamda";
+  //lambdafilepath = "/home/lrnqa/jenkins/workspace/Test-Lambda";
+  sh ( label:"Deploying to Lambda", script: """
+        set +x
+  aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
+  aws lambda publish-version --function-name $lamfunction_name
+   """)
 }

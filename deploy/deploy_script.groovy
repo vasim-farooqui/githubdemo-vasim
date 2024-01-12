4eddbdbd7c44a490e1c983c3afc7ddc631efd9aa
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-12 16:49:12 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,30 +1,34 @@
 #!groovy
-def function_name 
-{  
-   agent any
-   echo "function started stage2" 
-     
-   stage('deploy on lambda') { 
-            steps {
-                echo " deploy code on lambda "
-            }
-        }
-    
-} 
 
-def deployOnLAMBDA () {
-    //check
-  //******** Following Variables need to come from jenkins ********
-  //ECS_CLUSTER=qa-lrn-com
-  //ECS_SERVICE=lrn-legacy-cm-email-service
-  //TASK_DEFINATION=legacy-cm-email-service
-  //AWS_REGION=us-east-1
-  // Testing Jenkins Jira integration
-  lamfunction_name = "Jenkins-deployment-lamda";
-  lambdafilepath = "/home/lrnqa/jenkins/workspace/Test-Lambda";
-  sh ( label:"Deploying to Lambda", script: """
-        set +x
-  aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
-  aws lambda publish-version --function-name $lamfunction_name
-   """)
+pipeline {
+
+  agent { 
+    node {
+      label 'DEPLOY_TEST_LAMBDA'
+    }
+  }
+ 
+  environment {
+        lamfunction_name = 'Jenkins-deployment-lamda'
+        lambdafilepath = '/home/lrnqa/jenkins/workspace/Test-Lambda/lamdbacode.zip' // Path to your updated Lambda function code
+        }  
+	
+  stages {
+	  
+  stage('Code Deploy On lambda') { 
+      steps { 
+	      
+        echo " stage started "       
+        }
+     }
+   }	
 }
+def deployOnLAMBDA () {
+    sh """
+        echo "Deploying to Lambda" 
+		  
+		aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath   
+        
+    """
+}
+

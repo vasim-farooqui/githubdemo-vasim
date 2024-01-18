4322502b5fb8ad81c010536a755bacaba3080ab1
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-18 13:38:18 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -1,8 +1,11 @@
 def deployOnLAMBDA () {
-                        sh """
-                            echo "deployment has been start"      
+        
+                            sh ( label:"Deploying to Lambda", script: """
+        set +x
+  aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
+  aws lambda publish-version --function-name $lamfunction_name
+   """)
 	
-                           """
 }
 return this     
                 

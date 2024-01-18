d152c92407ff4f1c6d39d2cdec942c55a3ba3275
durgesh99lrn <143701693+durgesh99lrn@users.noreply.github.com>
2024-01-18 18:24:41 +0530

    Update deploy_script.groovy

--- deploy/deploy_script.groovy
+++ deploy/deploy_script.groovy
@@ -16,7 +16,7 @@ def deployOnLAMBDA () {
                 set +x
                 aws lambda update-function-code --function-name $lamfunction_name --zip-file fileb://$lambdafilepath
                 echo "Waiting For Version Update"
-                sleep (60)
+                sleep()
                 aws lambda publish-version --function-name $lamfunction_name
               """ )	
 }
